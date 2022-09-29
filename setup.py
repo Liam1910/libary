af6b57b4c18e565fd924621d2520c051980fb2ac
Liam1910 <108172258+Liam1910@users.noreply.github.com>
2022-09-29 21:19:40 +0200

    Update setup.py

--- setup.py
+++ setup.py
@@ -4,11 +4,11 @@ with open("README.md", "r", encoding="utf-8") as fh:
     long_description = fh.read()
 
 setuptools.setup(
-    name='libary',
-    version='0.1.0',
+    name='Calculating and PasswordGenerating Libary',
+    version='1.0.0',
     author='Liam Schmallowsky',
     author_email='liamschmallowsky123@gmail.com',
-    description='Testing installation of Package',
+    description='Can Calculate with Full and Decimal Numbers and can Generate Random 10 Character Long Passwords',
     long_description=long_description,
     long_description_content_type="text/markdown",
     url='https://github.com/Liam1910/libary',
