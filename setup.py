7c71b134b6942c1804f7e822bb98237f21dcc4f6
Liam1910 <108172258+Liam1910@users.noreply.github.com>
2022-10-01 15:40:05 +0000

    Update setup.py

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r", encoding="utf-8") as fh:
 
 setuptools.setup(
     name='Calculating,PasswordGeneratingLibary',
-    version='1.0.0',
+    version='1.0.1',
     author='Liam Schmallowsky',
     author_email='liamschmallowsky123@gmail.com',
     description='Can Calculate with Full and Decimal Numbers and can Generate Random 10 Character Long Passwords',
